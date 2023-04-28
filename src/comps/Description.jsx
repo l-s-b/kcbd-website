2d38455507403b8647f5915937734d9816bfdcef
l-s-b <lucio_sb@yahoo.com.ar>
2023-04-28 17:48:55 -0300

    v1.2.0 - Lotion, Frutilla back on stock, Product 404

--- src/comps/Description.jsx
+++ src/comps/Description.jsx
@@ -2,41 +2,44 @@ import React from 'react';
 import seedDetails from '../data/seed-details.json';
 
 export default function Description({data}) {
-    
-    const oilDescription = [
-        `Aceite de ${data.dosage} CBD de alta pureza`,
-        `Sabor ${data.flavor}`,
-        "Elaborado en California, Estados Unidos",
-        "Testeamos en Montevideo, Uruguay",
-        "Apto vegano",
-        "Apto celíacos",
-        "Envíos a todo el país"
-    ]
 
-    const seedDescription = seedDetails.filter(x => x.name === data.name)[0];
-    
-    const describeOil = data.type === 'oil' && 
-    <div className="flex col m1y h-fit autoY">
-        {oilDescription.map((x, index) => 
-            <span key={index}>{x}</span>
-        )}
-    </div>;
+    const notPets = ["Frutilla", "Menta", "Uva"].includes(data.flavor);
+    const foundSeedDesc = seedDetails.filter(x => x.name === data.name)[0]
+    const descriptions = {
+        "lotion": [
+            "Crema CBD Karst 50 gr.",
+            "Concentración CBD: 45%",
+            "Aroma neutro, apto para todo el cuerpo, ideal para el tratamiento localizado del dolor.",
+            "Apto vegano",
+            "Apto celíacos",
+            "Envíos a todo el país"
+        ],
+        "oil": [
+            `Aceite de ${data.concentration} CBD de alta pureza`,
+            `Sabor ${data.flavor}`,
+            "Elaborado en California, Estados Unidos",
+            "Testeamos en Montevideo, Uruguay",
+            "Ideal para dolores, ansiedad, tratamiento de cáncer y más patologías.",
+            notPets && "Apto vegano",
+            notPets && "Apto celíacos",
+            "Envíos a todo el país"
+        ],
+        "seedpack": foundSeedDesc ? foundSeedDesc.desc : []
+    }
 
-    const describeSeeds = data.type === 'seedpack' &&
-    <div className="flex col m1 h-fit autoY">
-        {seedDescription?.desc.map((x, index) => 
+    function describe(data) {
+    return <div className="flex col m1 h-fit autoY">
+        {descriptions[data.type].map((x, index) => 
             <p key={index}>{x}</p>
         )}
     </div>;
+    }
 
-    const hasDescription = data.type === 'oil' || seedDescription?.desc.length > 0
-
-    console.log('SD length: ', seedDescription?.desc.length)
+    const hasDescription = descriptions[data.type].length > 0
 
   return (
     hasDescription && <div className="bBox _hLimit _yFade br1 m1y">
-        {describeOil}
-        {describeSeeds}
+        {describe(data)}
     </div>
   )
 }
--- src/views/Product.jsx
+++ src/views/Product.jsx
@@ -1,47 +1,22 @@
-import { useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import data from '../data/variables.json';
 import products from '../data/products.json';
 import MuiNavbar from '../comps/MuiNavbar';
 import Footer from '../comps/Footer';
 import WB from '../comps/WhatsAppButton';
-import Image from '../comps/Image';
-import Counter from '../comps/Counter';
 import PageContainer from '../comps/PageContainer';
-import Description from '../comps/Description';
+import Found from '../comps/Product';
+import NotFound from '../comps/Product404';
 
 export default function Product() {
   const { id } = useParams();
   const myProduct = products.find(p => parseInt(p.id) === parseInt(id));
-  const [qty, setQty] = useState(1);
-  const handleCounter = count => {
-    setQty(count);
-  }
-  const handleCart = () => {
-    let cartMessage = `https://api.whatsapp.com/send?phone=${data.contact.phone}&text=${data.contact.cart}%0D%0AProducto: ${myProduct.name}%0D%0ACantidad: ${qty}%0D%0A%0D%0A*Precio total: $${myProduct.price * qty}*`
-    window.open(cartMessage, '_blank');
-  }
   
   return ( 
     <>
       <MuiNavbar />
-      <PageContainer showPattern={true}>
-        <div className="flex row wrap vw75 bg1 m2 centerX2 br2 h-fit z30">
-          <Image className="wrapped" fileName={myProduct.filename} alt={myProduct.name} />
-          <div className="wrapped centerXY flexCh centerX centerXch">
-            <div className="pad2 col">
-            <h2>{myProduct.name}</h2>
-            <h2>${myProduct.price}</h2>
-            <Description data={myProduct} />
-            <center id="centerX2" className="w-fit pill centerYch row wrap evenly dark-bg pad1 m1">
-              <span>Cantidad: </span>
-              <Counter qty={qty} changeFx={count => handleCounter(count)} className="flex row m1x" btn="bg1 pill hw2 pointer" />
-              <b>(${qty * myProduct.price})</b>
-            </center>
-            <button className="pad1 pill bg2 centerXY pointer" onClick={handleCart}>Enviar Pedido!</button>
-            </div>
-          </div>
-        </div>
+      <PageContainer showPattern={true} animation={!myProduct && "spin"}>
+        {myProduct ? <Found p={myProduct} /> : <NotFound /> }
       </PageContainer>
       <WB />
       <Footer />
